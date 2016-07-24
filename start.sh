ef93f536e961cfc64188ea388738b5a9de26bce9
Kimmo Kiiski <kimmo.kiiski@witchcraftlabs.com>
2016-07-24 22:26:38 +0300

    Reverted back to the more complex base image and start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,22 @@
-echo "Starting the simple test HTTP server"
-cd static
-python -m SimpleHTTPServer 80
+#!/bin/bash
+
+# Apply database migrations
+python manage.py migrate
+
+# python manage.py collectstatic --noinput  # Collect static files
+
+# Prepare log files and start outputting logs to stdout
+touch /srv/logs/gunicorn.log
+touch /srv/logs/access.log
+tail -n 0 -f /srv/logs/*.log &
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn coffeewatch.wsgi:application \
+    --name coffeewatch \
+    --bind 0.0.0.0:80 \
+    --workers 3 \
+    --log-level=info \
+    --log-file=/srv/logs/gunicorn.log \
+    --access-logfile=/srv/logs/access.log \
+    "$@"
