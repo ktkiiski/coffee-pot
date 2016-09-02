196e677f8d83719fc11df3de316f18587d2e62c1
Kimmo Kiiski <kimmo.kiiski@witchcraftlabs.com>
2016-09-02 22:34:47 +0300

    Upgraded Django to version 1.10

--- barista/settings.py
+++ barista/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'coffeestatus.apps.CoffeeStatusConfig',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.gzip.GZipMiddleware',
