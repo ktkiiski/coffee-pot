ec1040c095223d0a6a258a43050e4db170599941
Kimmo Kiiski <kimmo.kiiski@witchcraftlabs.com>
2016-09-04 20:02:46 +0300

    Serve files correctly (in development)

--- barista/urls.py
+++ barista/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
 from coffeestatus.views import CoffeeStatusView
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', CoffeeStatusView.as_view()),
 ]
+# Serve static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
