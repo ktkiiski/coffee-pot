557e620da7c928dbfc19c7e562df1190ae333b25
Kimmo Kiiski <kimmo.kiiski@witchcraftlabs.com>
2016-09-05 20:54:43 +0300

    Support for dummy image capturing with scipy

--- webcam/camera.py
+++ webcam/camera.py
@@ -3,26 +3,37 @@ from django.utils.timezone import utc
 from .models import Picture
 from io import BytesIO
 from datetime import datetime
-import picamera
+import scipy.misc
 import time
 
 
-def take_picture(self):
+def capture_image():
+    stream = BytesIO()
+    try:
+        import picamera
+    except Exception:
+        # Cannot import picamera, so let's use a dummy image instead
+        scipy.misc.imsave(stream, scipy.misc.face(), 'jpeg')
+    else:
+        with picamera.PiCamera() as camera:
+            camera.resolution = (320, 240)
+            # Camera warm-up time
+            time.sleep(2)
+            # Take the picture
+            camera.capture(stream, 'jpeg', quality=90)
+    return stream
+
+
+def take_picture():
     """
     Takes a picture with the Rasperry Pi's camera module,
     and saves the file and a Picture model record for it.
 
     Returns the saved Picture model instance.
     """
-    with picamera.PiCamera() as camera:
-        camera.resolution = (320, 240)
-        # Camera warm-up time
-        time.sleep(2)
-        # Take the picture
-        stream = BytesIO()
-        camera.capture(stream, 'jpeg', quality=90)
-        # Save the picture
-        now = datetime.now(utc)
-        image_filename = '{}.jpg'.format(now.strftime('%Y-%m-%d_%H.%M.%S'))
-        image_file = ImageFile(stream, name=image_filename)
-        return Picture.objects.create(image=image_file)
+    stream = capture_image()
+    # Save the picture
+    now = datetime.now(utc)
+    image_filename = '{}.jpg'.format(now.strftime('%Y-%m-%d_%H.%M.%S'))
+    image_file = ImageFile(stream, name=image_filename)
+    return Picture.objects.create(image=image_file)
