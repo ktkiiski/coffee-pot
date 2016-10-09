b05ee78e484f2362fbb61b75079abc9b4f698c33
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2016-10-09 17:24:15 +0300

    Upload snapshots to Amazon S3

--- barista/settings.py
+++ barista/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_s3_storage',
     'rest_framework',
 
     # Our own apps
@@ -172,3 +173,31 @@ SNAPSHOT_SCHEDULE_INTERVAL = int(os.environ.get('SNAPSHOT_SCHEDULE_INTERVAL') or
 SNAPSHOT_SCHEDULE_START_TIME = os.environ.get('SNAPSHOT_SCHEDULE_START_TIME') or '00:00'
 SNAPSHOT_SCHEDULE_END_TIME = os.environ.get('SNAPSHOT_SCHEDULE_END_TIME') or '23:59'
 SNAPSHOT_SCHEDULE_WEEKDAYS = os.environ.get('SNAPSHOT_SCHEDULE_WEEKDAYS') or '1,2,3,4,5,6,7'
+
+# Check if the Amazon S3 should be used for storing the camera snapshots
+try:
+    # The AWS access key used to access the storage buckets.
+    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+
+    # The AWS secret access key used to access the storage buckets.
+    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+
+    # The S3 bucket used to store uploaded files.
+    AWS_S3_BUCKET_NAME = os.environ["AWS_S3_BUCKET_NAME"]
+
+    # The region to connect to when storing files.
+    AWS_REGION = os.environ["AWS_REGION"]
+
+    # A prefix to add to the start of all uploaded files.
+    AWS_S3_KEY_PREFIX = os.environ.get("AWS_S3_KEY_PREFIX") or ""
+
+    # Whether to enable querystring authentication for uploaded files.
+    AWS_S3_BUCKET_AUTH = False
+
+except KeyError:
+    # Amazon S3 is not configured
+    pass
+
+else:
+    # If everything went fine, then use the S3 Storage instead of the default storage
+    DEFAULT_FILE_STORAGE = "django_s3_storage.storage.S3Storage"
