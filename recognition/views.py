56a70682ccef666b394ae3e98e1bdce49ccd871d
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2016-10-26 18:39:43 +0300

    Simple picture listing in the labelizer

--- recognition/views.py
+++ recognition/views.py
@@ -1,5 +1,18 @@
+"""
+Contains picture recognizion related view classes.
+"""
 from django.views.generic import TemplateView
+from webcam.models import Picture
 
 
 class LabelizerView(TemplateView):
+    """
+    Shows the HTML page for labelizing unlabeled pictures.
+    """
     template_name = "labelizer.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(LabelizerView, self).get_context_data(**kwargs)
+        pics = Picture.objects.filter(label__isnull=True)
+        context['pictures'] = pics.order_by('-created_at')[0:10]
+        return context
