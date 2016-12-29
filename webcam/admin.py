ba1f3bbd8c24184890d4f184bcac139263f8a285
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2016-12-29 22:02:13 +0200

    Added new fields to Django admin

--- webcam/admin.py
+++ webcam/admin.py
@@ -10,7 +10,12 @@ class AuthorAdmin(admin.ModelAdmin):
         'image',
         'width',
         'height',
-        'label',
+        'left_label',
+        'right_label',
+        'recognized_left_label',
+        'recognized_left_probability',
+        'recognized_right_label',
+        'recognized_right_probability',
     )
     readonly_fields = (
         'created_at',
