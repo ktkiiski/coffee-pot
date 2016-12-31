aecbb7b63aad95def95e3c87dbeb4cfb8736aa30
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2016-12-31 19:54:45 +0200

    Added label combinations to Django admin

--- recognition/admin.py
+++ recognition/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-from recognition.models import Label
+from recognition.models import Label, LabelCombination
 
 
 @admin.register(Label)
-class AuthorAdmin(admin.ModelAdmin):
+class LabelAdmin(admin.ModelAdmin):
     fields = (
         'id',
         'order',
         'title',
+        'description',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+
+
+@admin.register(LabelCombination)
+class LabelCombinationAdmin(admin.ModelAdmin):
+    fields = (
+        'left_label',
+        'right_label',
         'description',
         'created_at',
         'updated_at',
