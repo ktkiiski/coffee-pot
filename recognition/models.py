f7bb407eea0b6171fc41ad88973fe9e8c7ff50f6
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2016-12-31 19:48:25 +0200

    Added label combination model

--- recognition/models.py
+++ recognition/models.py
@@ -1,3 +1,4 @@
+from uuid import uuid1
 from django.db import models
 
 
@@ -32,3 +33,53 @@ class Label(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class LabelCombination(models.Model):
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid1,
+        editable=False,
+        verbose_name="Unique identifier",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Creation date/time",
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Last update date/time",
+    )
+    left_label = models.ForeignKey(
+        'recognition.Label',
+        on_delete=models.DO_NOTHING,
+        db_constraint=False,
+        related_name="+",
+        verbose_name="Left-side label",
+    )
+    right_label = models.ForeignKey(
+        'recognition.Label',
+        on_delete=models.DO_NOTHING,
+        db_constraint=False,
+        related_name="+",
+        verbose_name="Right-side label",
+    )
+    description = models.TextField(
+        verbose_name="Description",
+        help_text="What these two labels would be described together?",
+    )
+
+    def __str__(self):
+        return "{} & {}: {}".format(
+            self.left_label, self.right_label, self.description
+        )
+
+    class Meta:
+        unique_together = [
+            ('left_label', 'right_label'),
+        ]
+        ordering = [
+            'left_label__order',
+            'right_label__order',
+        ]
