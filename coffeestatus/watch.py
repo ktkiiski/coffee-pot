22591d55f5e677d295876f5abf86e25692a317a1
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2017-01-15 21:02:33 +0200

    Fixed the coffee freshness check

--- coffeestatus/watch.py
+++ coffeestatus/watch.py
@@ -37,29 +37,37 @@ def check_fresh_coffee(picture):
     range_end = picture.created_at
     range_start = range_end - FRESHNESS_DURATION
     # Left side
-    left_label_id = picture.recognized_left_label_id
-    left_prob = picture.recognized_left_probability
-    left_full = left_label_id in FULL_LABEL_IDS and left_prob >= FULL_CERTAINTY
-    left_queryset = Picture.objects.filter(
+    left_full_queryset = Picture.objects.order_by('created_at').filter(
+        created_at__gte=range_start,
+        created_at__lte=range_end,
+        recognized_left_label_id__in=FULL_LABEL_IDS,
+        recognized_left_probability__gte=FULL_CERTAINTY,
+    )
+    left_became_full = left_full_queryset.first() == picture
+    left_empty_queryset = Picture.objects.filter(
         created_at__gte=range_start,
         created_at__lt=range_end,
         recognized_left_label_id__in=EMPTY_LABEL_IDS,
         recognized_left_probability__gte=EMPTY_CERTAINTY,
     )
-    left_was_empty = left_queryset.exists()
-    left_is_fresh = left_full and left_was_empty
+    left_was_empty = left_empty_queryset.exists()
+    left_is_fresh = left_became_full and left_was_empty
     # Right side
-    right_label_id = picture.recognized_right_label_id
-    right_prob = picture.recognized_right_probability
-    right_full = right_label_id in FULL_LABEL_IDS and right_prob >= FULL_CERTAINTY
-    right_queryset = Picture.objects.filter(
+    right_full_queryset = Picture.objects.order_by('created_at').filter(
+        created_at__gte=range_start,
+        created_at__lte=range_end,
+        recognized_right_label_id__in=FULL_LABEL_IDS,
+        recognized_right_probability__gte=FULL_CERTAINTY,
+    )
+    right_became_full = right_full_queryset.first() == picture
+    right_empty_queryset = Picture.objects.filter(
         created_at__gte=range_start,
         created_at__lt=range_end,
         recognized_right_label_id__in=EMPTY_LABEL_IDS,
         recognized_right_probability__gte=EMPTY_CERTAINTY,
     )
-    right_was_empty = right_queryset.exists()
-    right_is_fresh = right_full and right_was_empty
+    right_was_empty = right_empty_queryset.exists()
+    right_is_fresh = right_became_full and right_was_empty
     # Determine message
     template = choice([
         "Guess what?! {} Go get some!",
